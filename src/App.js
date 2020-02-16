6f9d52db8791d8aa8b0a572e49ac38f3a20a07fd
6ry0u <6ry0u@tutanota.com>
2020-02-16 13:28:12 +0100

    feat: Render list of contract methods names

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import React, { Component } from 'react'
 // import {} from 'rimble-ui'
 // import './App.css';
 
+import contractWeb3 from './contract'
+
 class App extends Component {
+  state = {
+    contract: contractWeb3
+  }
+
+  // async componentDidMount () {
+  //   this.setState({contract: contractWeb3})
+  // }
+  renderList = obj => Object.keys(obj).map((key, index) => {
+    const re = /[a-z]+\(.*\)/gi
+    if (key.match(re)) {
+      return (
+        <div key={index}>{key}</div>
+      )
+    }
+  })
+  
   render () {
+    // console.log(this.state.contract.methods)
     return (
-      <div>Hello</div>
+      <div>
+        <h1>Unimask</h1>
+        <div>
+          {this.renderList(this.state.contract.methods)}
+        </div>
+      </div>
     )
   }
 }
